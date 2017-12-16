caf82d63aeb6b62909c102e024da19b5aab880aa
Oliver Bevan <oliverbevan23@gmail.com>
2017-12-16 19:35:03 +0000

    Recompile converter.js

--- converter.js
+++ converter.js
@@ -1,16 +1,81 @@
 function updateOutput() {
     let inputElement = document.getElementById("input");
+    let outputElement = document.getElementById("output");
+    let trimOutputElement = document.getElementById("trim-output");
+    outputElement.readOnly = false;
     let input = inputElement.value;
+    let output = "";
+    let alphabet = new RegExp("[A-z]");
+    let numbers = new RegExp("[0-9]");
     for (var i = 0; i < input.length; i++) {
-        if (1 == 1) {
-            // a-z or A-Z
+        let c = input.charAt(i);
+        if (alphabet.test(c)) {
+            output += ":regional_indicator_" + c.toString().toLowerCase() + ": ";
         }
-        else if (1 < 2) {
-            // 0-9
+        else if (numbers.test(c)) {
+            switch (c) {
+                case "1":
+                    output += ":one: ";
+                    break;
+                case "2":
+                    output += ":two: ";
+                    break;
+                case "3":
+                    output += ":three: ";
+                    break;
+                case "4":
+                    output += ":four: ";
+                    break;
+                case "5":
+                    output += ":five: ";
+                    break;
+                case "6":
+                    output += ":six: ";
+                    break;
+                case "7":
+                    output += ":seven: ";
+                    break;
+                case "8":
+                    output += ":eight: ";
+                    break;
+                case "9":
+                    output += ":nine: ";
+                    break;
+                case "0":
+                    output += ":zero: ";
+                    break;
+            }
         }
-        else if (1 > 3) {
+        else if (c == " ") {
+            output += "   ";
+        }
+        else if (c == "*") {
+            output += ":asterisk: ";
+        }
+        else if (c == "~") {
+            output += ":wavy_dash: ";
+        }
+        else if (c == '-' || c == '–' || c == '—' || c == '−') {
+            output += ":heavy_minus_sign: ";
+        }
+        else if (c == "+") {
+            output += ":heavy_plus_sign: ";
+        }
+        else if (c == "#") {
+            output += ":hash: ";
+        }
+        else if (c == "!") {
+            output += ":exclamation: ";
+        }
+        else if (c == "?") {
+            output += ":question: ";
         }
     }
+    if (trimOutputElement.checked && output.length > 2000) {
+        output = output.slice(0, 2000);
+    }
+    document.getElementById("output").innerHTML = output;
+    outputElement.readOnly = true;
 }
 function copyToClipboard() {
     let outputElement = document.getElementById("output");
