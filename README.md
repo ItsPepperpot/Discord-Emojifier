14a99f778961d38785f03062b447ec650558ae72
Oliver Bevan <oliverbevan23@gmail.com>
2017-12-16 13:34:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Discord Emojifier
+## Introduction
+It's the program you've been waiting for. The official not-official Discord text-to-emoji converter.
+## How to Use
+1. Simply enter your text into the top text box.
+2. Copy the emoji from the emoji output, or the raw text output.
+3. Paste into Discord and enjoy annoying everyone.
+😀
---
+++ converter.ts
@@ -0,0 +1,21 @@
+function updateOutput() {
+    let inputElement = <HTMLTextAreaElement>document.getElementById("input");
+    let input = inputElement.value;
+
+    for (var i = 0; i < input.length; i++) {
+        if () {
+            // a-z or A-Z
+
+        } else if () {
+            // 0-9
+
+        } else if () {
+
+        }
+    }
+}
+
+function copyToClipboard() {
+    let outputElement = <HTMLTextAreaElement>document.getElementById("output");
+    let output = outputElement.value;
+}
