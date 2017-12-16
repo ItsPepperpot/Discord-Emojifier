896f5378b92e2a653e89a7f1f75c606a3b3c18bc
Oliver Bevan <oliverbevan23@gmail.com>
2017-12-16 19:32:01 +0000

    Implement text conversion algorithm. Remove random emoji generation

--- converter.ts
+++ converter.ts
@@ -1,18 +1,56 @@
 function updateOutput() {
     let inputElement = <HTMLTextAreaElement>document.getElementById("input");
+    let outputElement = <HTMLTextAreaElement>document.getElementById("output");
+    let trimOutputElement = <HTMLInputElement>document.getElementById("trim-output");
+    outputElement.readOnly = false;
     let input: string = inputElement.value;
+    let output: string = "";
+
+    let alphabet = new RegExp("[A-z]");
+    let numbers = new RegExp("[0-9]");
 
     for (var i = 0; i < input.length; i++) {
-        if (1 == 1) {
-            // a-z or A-Z
+        let c = input.charAt(i);
 
-        } else if (1 < 2) {
-            // 0-9
-
-        } else if (1 > 3) {
-
+        if (alphabet.test(c)) {
+            output += ":regional_indicator_" + c.toString().toLowerCase() + ": ";
+        } else if (numbers.test(c)) {
+            switch(c) {
+                case "1": output += ":one: "; break;
+                case "2": output += ":two: "; break;
+                case "3": output += ":three: "; break;
+                case "4": output += ":four: "; break;
+                case "5": output += ":five: "; break;
+                case "6": output += ":six: "; break;
+                case "7": output += ":seven: "; break;
+                case "8": output += ":eight: "; break;
+                case "9": output += ":nine: "; break;
+                case "0": output += ":zero: "; break;
+            }
+        } else if (c == " ") { // Space
+            output += "   ";
+        } else if (c == "*") { // Asterisk
+            output += ":asterisk: ";
+        } else if (c == "~") { // Tilde
+            output += ":wavy_dash: ";
+        } else if (c == '-' || c == '–' || c == '—' || c == '−') {  // Hypen/En dash/Em dash/Minus sign
+            output += ":heavy_minus_sign: ";
+        } else if (c == "+") { // Plus sign
+            output += ":heavy_plus_sign: ";
+        } else if (c == "#") { // Hash
+            output += ":hash: ";
+        } else if (c == "!") { // Exclamation mark
+            output += ":exclamation: ";
+        } else if (c == "?") { // Question mark
+            output += ":question: ";
         }
     }
+    if (trimOutputElement.checked && output.length > 2000)
+    {
+        output = output.slice(0, 2000);
+    }
+    document.getElementById("output").innerHTML = output;
+    outputElement.readOnly = true;
 }
 
 function copyToClipboard() {
@@ -26,15 +64,4 @@ function copyToClipboard() {
     } catch {
         alert("Oops, unable to copy. :(");
     }
-}
-
-window.onload = function() { 
-    let emojiTitle: string = "";
-
-    for (var i = 0; i < 4; i++)
-    {
-        emojiTitle += String.fromCharCode(Math.floor(Math.random() * (128309 - 128512)) + 128512);
-    }
-
-    document.getElementById("emoji-title").innerHTML = emojiTitle;
 }
